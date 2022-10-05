43e74330aa67307b195c61a1afc0e9edf857c0a0
Ha <jhy000714@gmail.com>
2022-10-05 16:13:12 +0900

    초기화 버튼(입력하고 있던 창을 초기화함), 비우기버튼 (지금까지 입력한 쿼리문을 삭제함)

---
+++ src/App.js
@@ -0,0 +1,60 @@
+import './App.css';
+import { useState } from 'react';
+
+function App() {
+  const [postgresql, setPostgresql] = useState([]);
+  const [inputValue, setInputValue] = useState([]);
+  const onReset = () => {
+    setInputValue("");
+  };
+  const onPageReset = () => {
+    const remove = document.getElementById('query_sentences');
+    remove.innerHTML="";
+  };
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    setPostgresql((current) => { 
+
+      /* FROMDUAL 변환작업 */
+      if (/from dual *;/i.test(inputValue) === true) {
+        var queryNotIncludeDual = inputValue.split(/FROM/i);
+        var postgresqlQuery = queryNotIncludeDual[0];
+        return [...current, {
+          value: `${postgresqlQuery};`, 
+        },];
+      }
+      else return [...current, {
+        value: inputValue, 
+      },];
+      
+    });
+
+    setInputValue("");
+    
+  };
+
+  return (
+    <div>
+      <p id="title">Query Converter</p>
+      <form id="oracle-query" onSubmit={handleSubmit}>
+        <input type="text" 
+        
+        value ={inputValue}
+          onChange={(event) => {
+            setInputValue(event.target.value);
+        }} />
+        <button type="submit">변환</button>
+        <button onClick={onReset}>초기화</button>
+      </form>
+      
+      <ol id="query_sentences">{postgresql.map((item) => (
+        <div id="query_sentence">
+          {item.value}
+        </div>
+      ))}</ol>
+      <button onClick={onPageReset}>비우기</button>
+    </div>
+  );
+}
+
+export default App;
