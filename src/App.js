1b822dc1ba3d5d3c0c374992a9c59d8ec16d6b17
Hayeon-Jung <jhy00714@gmail.com>
2022-10-26 10:26:51 +0900

    기본코드

--- src/App.js
+++ src/App.js
@@ -4,9 +4,6 @@ import { useState } from 'react';
 function App() {
   const [postgresql, setPostgresql] = useState([]);
   const [inputValue, setInputValue] = useState([]);
-  const onReset = () => {
-    setInputValue("");
-  };
   const onPageReset = () => {
     const remove = document.getElementById('query_sentence');
     remove.innerHTML="";
@@ -14,19 +11,17 @@ function App() {
   const handleSubmit = (event) => {
     event.preventDefault();
     setPostgresql((current) => { 
+      var qStr = inputValue;
+      // FROMDUAL 변환작업 
+      qStr = qStr.replace(/FROM\s+DUAL/igs, "");
 
-      /* FROMDUAL 변환작업 */
-      if (/from dual *;/i.test(inputValue) === true) {
-        var queryNotIncludeDual = inputValue.split(/FROM/i);
-        var postgresqlQuery = queryNotIncludeDual[0];
-        return [...current, {
-          value: `${postgresqlQuery};`, 
-        },];
-      }
-      else return [...current, {
-        value: inputValue, 
+      // 함수이름 변환작업
+      // qStr = qStr.작업 코드;
+
+      return [...current, {
+        value: `${qStr}`, 
       },];
-      
+
     });
 
     setInputValue("");
@@ -34,7 +29,7 @@ function App() {
   };
 
   return (
-    <div>
+    <div id="container">
       <p id="title">Query Converter</p>
       <form id="oracle-query" onSubmit={handleSubmit}>
         <input type="text" 
@@ -43,8 +38,7 @@ function App() {
           onChange={(event) => {
             setInputValue(event.target.value);
         }} />
-        <button type="submit">변환</button>
-        <button onClick={onReset}>초기화</button>
+        <button id="input_button" type="submit">변환</button>
       </form>
       
       <ol id="query_sentence">{postgresql.map((item) => (
@@ -52,7 +46,7 @@ function App() {
           {item.value}
         </div>
       ))}</ol>
-      <button onClick={onPageReset}>비우기</button>
+      <button id='page-reset' onClick={onPageReset}>비우기</button>
     </div>
   );
 }
