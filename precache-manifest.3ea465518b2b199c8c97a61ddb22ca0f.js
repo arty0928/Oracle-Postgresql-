aba73ed45112ba67159bf7174ac51b29ecdeb5ee
Hayeon-Jung <jhy00714@gmail.com>
2022-10-28 16:40:09 +0900

    Updates

---
+++ precache-manifest.3ea465518b2b199c8c97a61ddb22ca0f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "48906a90569bf2ade6f5",
+    "url": "/Oracle-Postgresql-/static/css/main.f19a41ad.chunk.css"
+  },
+  {
+    "revision": "48906a90569bf2ade6f5",
+    "url": "/Oracle-Postgresql-/static/js/main.48906a90.chunk.js"
+  },
+  {
+    "revision": "c9ee82573af08d8ed619",
+    "url": "/Oracle-Postgresql-/static/js/1.c9ee8257.chunk.js"
+  },
+  {
+    "revision": "d56734a240a1361fc67b",
+    "url": "/Oracle-Postgresql-/static/js/runtime~main.d56734a2.js"
+  },
+  {
+    "revision": "a8cf65b6bd74f135d83d6c919c148301",
+    "url": "/Oracle-Postgresql-/index.html"
+  }
+];
