bc4dc593e68469fd4755bf1cacb36dece8b28fd5
Ha <109961807+Hayeon-Jung@users.noreply.github.com>
2022-11-04 15:54:17 +0900

    main

--- src/components/QueryInput.js
+++ src/components/QueryInput.js
@@ -56,11 +56,11 @@ export default function QueryInput() {
             type="text"
             rows="2"
             required
-            className="
-        relative block w-full appearance-none rounded-none rounded-t-md
-        px-3 py-2 text-gray-900 placeholder-gray-500 border-b
-        focus:z-10 focus:outline-none 
-        sm:text-sm"
+            className="bg-gray-100
+            relative block w-full appearance-none rounded-none rounded-t-md
+            px-3 py-2 text-gray-900 placeholder-gray-500 border-b
+            focus:z-10 focus:outline-none 
+            sm:text-sm"
             placeholder="input oracle query"
             value={inputValue}
             onChange={(event) => {
