8b00ac3d788e59a3afccbb4b3bd72bad73a90876
Hannah Kim <65204055+mylotto0626@users.noreply.github.com>
2022-11-15 17:08:37 +0900

    memo

--- src/Pages/Functions copy.js
+++ src/Pages/Functions copy.js
@@ -532,12 +532,312 @@ function Functions() {
                 </div>
               </details>
             </div>
-            <p
-              class="mt-3 text-center sm:text-2xl sm:font-bold tracking-tight text-gray-900
-                         text-3xl font-bold text-center title-font text-gray-900 mb-4"
-            >
-              Functions currently under consideration
-            </p>
+
+            <div class="w-full lg:w-1/2 px-4 py-2">
+              <details className="mb-4 bg-gray-200 rounded-md py-2 px-4">
+                <summary className="font-semibold leading-7">
+                  FIRST/LAST
+                </summary>
+
+                <div className="pl-4">
+                  <p>
+                    postgres에서는 first,last 함수를 제공하지 않아 프로시저
+                    함수를 추가해야만 동일한 기능 사용 가능
+                  </p>
+                </div>
+              </details>
+
+              <details className="mb-4 bg-gray-200 rounded-md py-2 px-4">
+                <summary className="font-semibold leading-7">NEXT_DAY</summary>
+
+                <div className="pl-4">
+                  <ul className="ml-5 leading-7">
+                    <li>
+                      <p className="p-4 text-pink-700">
+                        SELECT 'TOMORROW'::date + ( 4 + 7 - extract ( dow FROM
+                        'TOMORROW'::date))::int%7;
+                      </p>
+                    </li>
+                    <li>
+                      <p className="p-4 text-pink-700">
+                        SELECT (date_trunc('MONTH', ('20130312')::timestamp) +
+                        INTERVAL '1 MONTH - 1 day')::timestamp
+                      </p>
+                    </li>
+                  </ul>
+                </div>
+              </details>
+
+              <details className="mb-4 bg-gray-200 rounded-md py-2 px-4">
+                <summary className="font-semibold leading-7">
+                  XML 관련 함수
+                </summary>
+
+                <div className="pl-4">
+                  <ul className="ml-5 leading-7">
+                    <li>EXTRACT(XML)</li>
+                    <li>INCERTXMLBEFORE</li>
+                    <li>EXISTNODE</li>
+                    <li>INCERTCHILDXML</li>
+                    <li>APPENDCHILDXML </li>
+                    <li>EXTRACTVALUE</li>
+                    <li>DELETEXML</li>
+                  </ul>
+                  <br></br>
+                  <ol className="ml-5 leading-7">
+                    <li>
+                      Windows 10 검색 영역→PostgreSQL 데이터베이스 GUI PgAdmin을
+                      시작{" "}
+                    </li>
+                    <li>
+                      서버 데이터베이스 암호를 추가→왼쪽 "서버"
+                      옵션→데이터베이스(예: Postgres)를 확장
+                    </li>
+                    <li>
+                      특정 데이터베이스에 대한 쿼리 도구 아이콘을 실행 "XPath"
+                      기능을 사용하려면 XML 데이터를 저장할 XML 유형 열이 포함된
+                      테이블이 있어야 합니다.
+                    </li>
+                    <li>
+                      {" "}
+                      CREATE TABLE postgresql 명령을 사용하여 새 테이블을 생성.
+                      "ID" 열은 정수 유형이고 "정보" 열은 XML 데이터를 저장하기
+                      위한 "XML" 유형입니다. PgAdmin "실행" 버튼을 사용하여 이
+                      쿼리를 실행
+                    </li>
+                    <li>
+                      쿼리에서 XPath() 함수를 사용하여 XML 데이터를 가져오기
+                      위해 데이터베이스의 SELECT 명령어 내에서 XPath 기능을
+                      활용해야 합니다. XPath() 함수는 일반적으로 세 개의 인수를
+                      취합니다.
+                      <ol className="ml-5 leading-7">
+                        <li>
+                          첫 번째 인수 : XML 데이터의 노드 집합/태그에 대해
+                          알려주는 XPath 표현식=경로{" "}
+                        </li>
+                        <li>
+                          두 번째 인수는 XML 데이터가 있는 실제 데이터 또는 XML
+                          열.
+                        </li>
+                      </ol>
+                    </li>
+                  </ol>
+                </div>
+              </details>
+
+              <details className="mb-4 bg-gray-200 rounded-md py-2 px-4">
+                <summary className="font-semibold leading-7">
+                  NLS 관련 함수
+                </summary>
+
+                <div className="pl-4">
+                  <p>postgresql.conf 설정 파일</p>
+                  <p className="font-bold leading-7">
+                    {" "}
+                    유저 선택 언어로 메세지가 번역되려면 NLS 사용<br></br>
+                    다른 지역 지원 기능과는 독립
+                  </p>
+                  <ul className="ml-5 leading-7">
+                    <li>
+                      - initdb --locale : 다른 지역(locale)을 사용하고 싶은 경우
+                      <br></br>
+                      &emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&ensp;시스템의
+                      로케일 설정이 불분명한 경우
+                    </li>
+                    <li>
+                      locale -a : 지역 지원 기능이 정상적으로 동작하지 않는 경우
+                      <br></br>&emsp;&emsp;&emsp;&emsp;&ensp;operating system 이
+                      제공하는 경우에 커멘드를 사용
+                    </li>
+                    <li>
+                      PostgreSQL JDBC : MULE_INTERNAL,LATIN6,LATIN8, LATIN10 를
+                      지원하지 않음
+                    </li>
+                    <li>
+                      PostgreSQL character set : ISO 8859-싱글 바이트 문자
+                      <br></br>
+                      EUC(확장 Unix 코드), UTF-8, Mule 내부 코드와(멀티 바이트
+                      character set)
+                    </li>
+                    <li>
+                      디폴트 character set : PostgreSQL 데이터베이스 클러스터가
+                      초기화되어질 때 결정.
+                      <br></br>데이터베이스를 생성할 때 오버라이드.각각 다른
+                      character set 를 가지는 다수의 데이터베이스를 가질 수
+                      있습니다.
+                    </li>
+                    <li>
+                      PostgreSQL 서버-클라이언트 character set 자동적으로 변환
+                    </li>
+                    <li>변환 정보는 pg_conversion 시스템 카탈로그에 저장.</li>
+                    <li>
+                      새로운 변환을 작성하려면 SQL 커멘드의 CREATE CONVERSION 를
+                      사용
+                    </li>
+                    <li>LC_COLLATE 와 LC_CTYPE 설정 : initdb 시간에 결정</li>
+                  </ul>
+                  <br></br>
+                  <p className="font-bold leading-7">
+                    재실행하지 않는 한 변경할 수 없습니다.
+                    <br></br>LC_MESSAGES 나 LC_MONETARY 등 다른 지역 설정은 서버
+                    환경에 의해 처음으로 결정되지만 변경 가능
+                  </p>
+                  <p className="p-4 text-pink-700">
+                    char *pg_encoding_to_char(int encoding_id)<br></br>
+                    int PQclientEncoding(const PGconn * conn );<br></br>
+                    SET CLIENT_ENCODING TO ' · value
+                  </p>
+                </div>
+              </details>
+
+              <details className="mb-4 bg-gray-200 rounded-md py-2 px-4">
+                <summary className="font-semibold leading-7">CAST</summary>
+
+                <div className="pl-4">
+                  <p>
+                    CAST 함수를 MULTISET과 함께 사용하는 경우가 있습니다.
+                    <br></br>
+                    PostgreSQL은 MULTISET 기능을 지원하지 않기 때문에<br></br>
+                    사용자가 직접 MULTISET 내부 행을 분리하여 각각의 형변환을
+                    해야 합니다.
+                  </p>
+                </div>
+              </details>
+
+              <details className="mb-4 bg-gray-200 rounded-md py-2 px-4">
+                <summary className="font-semibold leading-7">ASCIISTR</summary>
+
+                <div className="pl-4">
+                  <p>
+                    ASCII 이외의 문자는 아래와 같은 방법으로 \xxxx형태로 표현할
+                    수 있습니다.
+                  </p>
+                  <p className="p-4 text-pink-700">
+                    concat('\',lpad(to_hex(ascii('Ä')),4,'0'))
+                  </p>
+                </div>
+              </details>
+
+              <details className="mb-4 bg-gray-200 rounded-md py-2 px-4">
+                <summary className="font-semibold leading-7">CONVERT</summary>
+
+                <div className="pl-4">
+                  <ul className="ml-5 leading-7">
+                    <li>
+                      {" "}
+                      ORACLE 지원 문자셋
+                      <a
+                        href="https://docs.oracle.com/cd/B28359_01/server.111/b28286/functions027.htm#SQLRF00620"
+                        target="blank"
+                      ></a>
+                    </li>
+                    <li>
+                      {" "}
+                      PostgreSQL 지원 문자셋
+                      <a
+                        href="https://www.postgresql.org/docs/current/multibyte.html"
+                        target="blank"
+                      ></a>
+                    </li>
+                  </ul>
+                </div>
+              </details>
+
+              <details className="mb-4 bg-gray-200 rounded-md py-2 px-4">
+                <summary className="font-semibold leading-7">XMLQUERY</summary>
+
+                <div className="pl-4">
+                  <ul className="ml-5 leading-7">
+                    <li>
+                      {" "}
+                      <p className="p-4 text-pink-700">
+                        XPATH(xpath, xml [, nsarray])
+                      </p>{" "}
+                    </li>
+                    <li>
+                      {" "}
+                      <p className="p-4 text-pink-700">
+                        SELECT XPATH('//employees/employee/name', xmldata) FROM
+                        Employees;
+                      </p>
+                    </li>
+                  </ul>
+                </div>
+              </details>
+
+              <details className="mb-4 bg-gray-200 rounded-md py-2 px-4">
+                <summary className="font-semibold leading-7">UPDATEXML</summary>
+
+                <div className="pl-4">
+                  <p className="p-4 text-pink-700">
+                    UPDATE xdata SET xmlcode='<values>Infosys-Bangalore</values>
+                    '<br></br>
+                    WHERE cast(xpath('//values/text()',xmlcode) AS text[]) = '
+                    Infosys';
+                  </p>
+                  <ul className="ml-5 leading-7">
+                    <li>
+                      <p className="font-bold leading-7">
+                        쿼리 실행하기 전에 스키마 DB에 미리 테이블이랑 값을
+                        넣어둔 것( 쿼리가 가져와서 쓸 수 있게 한 것)
+                      </p>
+                    </li>
+                  </ul>
+                  <p className="p-4 text-pink-700">
+                    CREATE TABLE xdata( id INT, xmlcode XML);<br></br>
+                    INSERT INTO xdata VALUES (1,'<values>Infosys</values>');
+                    <br></br>
+                    INSERT INTO xdata VALUES (2,'<values>Enterprisedb</values>
+                    ');<br></br>
+                    INSERT INTO xdata VALUES (3,'<values>Wipro</values>');
+                  </p>
+                </div>
+              </details>
+
+              <details className="mb-4 bg-gray-200 rounded-md py-2 px-4">
+                <summary className="font-semibold leading-7">
+                  XMLSERIALIZE
+                </summary>
+
+                <div className="pl-4">
+                  <p className="font-bold leading-7">
+                    xml에서 문자열 값을 생성하는 반대의 작업은 xmlserialize
+                    함수를 사용
+                  </p>
+                  <p className="p-4 text-pink-700">
+                    XMLSERIALIZE ( [ DOCUMENT | CONTENT ]<br></br>
+                    *value* AS *type* )<br></br>
+                  </p>
+                  <p>
+                    PostgreSQL에서는 단순히 값을 캐스팅하는 것이 가능합니다.
+                    <br></br> XMLPARSE 및 XMLSERIALIZE를 사용하지 않고 문자열
+                    값과 xml 사이를 <br></br>캐스트하면 DOCUMENT인지 또는
+                    CONTENT인가 하는 선택이 "XML 옵션" 세션 구성 매개 변수에
+                    의해 결정됩니다.
+                  </p>
+                  <p className="p-4 text-pink-700">
+                    SELECT xmlserialize (DOCUMENT (<br></br>
+                    SELECT xmlroot(<br></br>
+                    xmlelement(name root, xmlelement(name value, 'test') ),
+                    <br></br>
+                    version '1.0', standalone yes) ) AS text);
+                  </p>
+                </div>
+              </details>
+
+              <details className="mb-4 bg-gray-200 rounded-md py-2 px-4">
+                <summary className="font-semibold leading-7">TO_LOB</summary>
+
+                <div className="pl-4">
+                  <p>
+                    Postgresql은 Oracle의 LONG, LONG RAW data type을 지원하지
+                    않습니다.<br></br>비슷한 data type으로는 long(charater data)
+                    는 text(단 text는 1gb limit , long은 2gb limit) , long
+                    raw(binary data) 는 bytea로 변환이 가능할 것으로 보입니다
+                  </p>
+                </div>
+              </details>
+            </div>
           </div>
         </div>
       </section>
