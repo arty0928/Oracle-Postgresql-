508470beb349cc60f3d5814fc53d10e3ee8a4fcf
Ha <109961807+Hayeon-Jung@users.noreply.github.com>
2022-11-17 10:36:18 +0900

    functions page

--- src/Pages/Functions.js
+++ src/Pages/Functions.js
@@ -655,12 +655,6 @@ function Functions() {
               </details>
 
             </div>
-            <p
-              className="mt-3 text-center sm:text-2xl sm:font-bold tracking-tight text-gray-900
-                         text-3xl font-bold text-center title-font text-gray-900 mb-4"
-            >
-              Functions currently under consideration
-            </p>
           </div>
         </div>
       </section>
