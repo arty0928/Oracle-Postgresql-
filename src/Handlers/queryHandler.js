cd2988ae7fb825d16108d0073d6240ab4e350971
Ha <109961807+Hayeon-Jung@users.noreply.github.com>
2022-11-23 11:00:31 +0900

    query

--- src/Handlers/queryHandler.js
+++ src/Handlers/queryHandler.js
@@ -505,10 +505,10 @@ export default function ora2pg(Qstr) {
   }
 
   //PARTITION 뒤에 오는 'TIMESTAMP'라는 단어를 소거함... 조건을 이렇게 줘도 괜찮을지 모르겠네요
-  Qstr = Qstr.replace(/(?<=PARTITION+(?:.*))\bTIMESTAMP+/gis, (match) => {
-    changedList.push(match);
-    return "";
-  });
+  // Qstr = Qstr.replace(/(?<=PARTITION+(?:.*))\bTIMESTAMP+/gis, (match) => {
+  //   changedList.push(match);
+  //   return "";
+  // });
 
   //removes YEAR precision from INTERVAL type in table definition.
   Qstr = Qstr.replace(
